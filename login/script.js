ac95a9d3a216423981665686eec8dc858ce055cf
Gabriel Majeau <127263958+gabmajeau@users.noreply.github.com>
2025-06-03 15:22:11 -0300

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,41 @@
+let card = document.querySelector(".card");
+let loginbutton = document.querySelector(".loginbutton");
+let cadastrobutton = document.querySelector(".cadastrobutton");
+
+loginbutton.onclick = () => {
+    card.classList.remove("cadastroActive")
+    card.classList.add("loginActive")
+}
+
+cadastrobutton.onclick = () => {
+    card.classList.remove("loginActive")
+    card.classList.add("cadastroActive")
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.getElementById("email");
+    const senhaInput = document.getElementById("senha");
+    const loginBtn = document.querySelector("button[onclick='loginUsuario()']");
+  
+    /* Ativa o botão de login só quando os campos estiverem preenchidos */
+    const verificarCampos = () => {
+      if (emailInput.value && senhaInput.value) {
+        loginBtn.disabled = false;
+      } else {
+        loginBtn.disabled = true;
+      }
+    };
+  
+    emailInput.addEventListener("input", verificarCampos);
+    senhaInput.addEventListener("input", verificarCampos);
+  });
+  
+function voltarHome() {
+    window.location.href = "../home.html"; // ajuste o nome da página anterior se for diferente
+}
+
+
